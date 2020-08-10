6fcb5d1d15dad831deadad2c15d06f27649e96ed
Ogochukwu Mark Nzei <mnzei@ecobank.com>
2020-08-10 21:04:55 +0100

    Using PreAuthorize()

--- src/main/java/com/mark/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mark/demo/security/ApplicationSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.mark.demo.security;
 
+import com.mark.demo.student.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,10 +15,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.mark.demo.security.ApplicationUserPermission.*;
 import static com.mark.demo.security.ApplicationUserRoles.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -36,9 +41,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() //
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(ADMIN.name())
+//                .antMatchers(HttpMethod.DELETE,"management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST,"management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -51,18 +61,29 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails markNzeiUser = User.builder()
                 .username("marknzei")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) // ROLE_STUDENT
+                //.roles(STUDENT.name()) // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails adminUser = User.builder()
                 .username("joanna")
                 .password(passwordEncoder.encode("kanyinsola"))
-                .roles(ADMIN.name())
+                //.roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("tomtom"))
+                //.roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+
 
         return new InMemoryUserDetailsManager(
                 markNzeiUser,
-                adminUser
+                adminUser,
+                tomUser
         );
     }
 }
--- src/main/java/com/mark/demo/security/ApplicationUserRoles.java
+++ src/main/java/com/mark/demo/security/ApplicationUserRoles.java
@@ -1,14 +1,18 @@
 package com.mark.demo.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.mark.demo.security.ApplicationUserPermission.*;
 
 public enum  ApplicationUserRoles {
     STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE));
+    ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ,STUDENT_READ));
 
     private final Set<ApplicationUserPermission> permissions;
 
@@ -17,6 +21,13 @@ public enum  ApplicationUserRoles {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream().map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
